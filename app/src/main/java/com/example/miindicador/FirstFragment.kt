daed4203c99ed0827f7915b3d5cbd6007af17ddd
SebaChuaqui <67081436+SebaChuaqui@users.noreply.github.com>
2020-12-07 15:51:16 -0300

    Intento 4

--- app/src/main/java/com/example/miindicador/FirstFragment.kt
+++ app/src/main/java/com/example/miindicador/FirstFragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.miindicador.local.IndicadoresEntity
+import com.example.miindicador.repo.IndicadoresAdapter
+import com.example.miindicador.repo.IndicadoresViewModel
+import com.example.miindicador.repo.PassTheData
+import kotlinx.android.synthetic.main.fragment_first.*
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), PassTheData {
+
+    private val model: IndicadoresViewModel by activityViewModels()
+    lateinit var mAdapter: IndicadoresAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mAdapter = IndicadoresAdapter(this)
+    }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -23,9 +35,38 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val mRecyclerView = RecyclerViewIndicadores
+        RecyclerViewIndicadores.adapter = mAdapter
+        RecyclerViewIndicadores.layoutManager = LinearLayoutManager(context)
+        model.liveDataFromLocal.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                mAdapter.updateAdapter(it)
+            }
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        })
+
+    }
+
+    override fun passTheIndicadores(indicadores: IndicadoresEntity) {
+
+        val mBundle = Bundle()
+        mBundle.putString("id", indicadores.id.toString())
+        mBundle.putString("bitcoin", indicadores.bitcoinnombre)
+        mBundle.putString("bitcoin", indicadores.dolarnombre)
+        mBundle.putString("bitcoin", indicadores.dolarIntercambionombre)
+        mBundle.putString("bitcoin", indicadores.euronombre)
+        mBundle.putString("bitcoin", indicadores.imacecnombre)
+        mBundle.putString("bitcoin", indicadores.ipcnombre)
+        mBundle.putString("bitcoin", indicadores.ivpnombre)
+        mBundle.putString("bitcoin", indicadores.libracobrenombre)
+        mBundle.putString("bitcoin", indicadores.tasadesempleonombre)
+        mBundle.putString("bitcoin", indicadores.tpmnombre)
+        mBundle.putString("bitcoin", indicadores.ufnombre)
+        mBundle.putString("bitcoin", indicadores.utmnombre)
+
+
+        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, mBundle)
+
+
     }
 }
